6168b3dfb349135c81b1d2656b725aeacb35ace9
Jan Kuehle <jkuehle90@gmail.com>
2019-12-14 22:23:20 +0000

    Add Docker image caching

--- scripts/docker-build.ps1
+++ scripts/docker-build.ps1
@@ -1,1 +1,19 @@
-docker build -t github-status-stats .
+if ($(git diff --stat))
+{
+    $TAG = "dev"
+}
+else
+{
+    $TAG = $(git rev-parse HEAD)
+}
+
+docker pull frigus02/github-status-stats-build
+
+docker build --cache-from=frigus02/github-status-stats-build --target build -t frigus02/github-status-stats-build .
+docker build --cache-from=frigus02/github-status-stats-build -t frigus02/github-status-stats:$TAG .
+
+if ($TAG -ne "dev")
+{
+    docker push frigus02/github-status-stats-build
+    docker push frigus02/github-status-stats:$TAG
+}
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 set -eu
-docker build -t github-status-stats .
+
+if [ "$(git diff --stat)" != "" ]; then
+    TAG="dev"
+else
+    TAG=$(git rev-parse HEAD)
+fi
+
+docker pull frigus02/github-status-stats-build
+
+docker build --cache-from=frigus02/github-status-stats-build --target build -t frigus02/github-status-stats-build .
+docker build --cache-from=frigus02/github-status-stats-build -t "frigus02/github-status-stats:$TAG" .
+
+if [ "$TAG" != "dev" ]; then
+    docker login -u frigus02 -p "$DOCKER_PASSWORD"
+    docker push frigus02/github-status-stats-build
+    docker push "frigus02/github-status-stats:$TAG"
+fi
