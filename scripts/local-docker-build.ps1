91ba2a17c98687a092c1d53d7ce1a9700f23a5b4
Jan Kuehle <jkuehle90@gmail.com>
2020-02-23 16:56:19 +0000

    Fail local build on error

--- scripts/local-docker-build.ps1
+++ scripts/local-docker-build.ps1
@@ -1,3 +1,12 @@
+$ErrorActionPreference = "Stop"
+function ExitIfNativeCallFailed($NativeCallSuccess)
+{
+    if (-not $NativeCallSuccess)
+    {
+        throw 'error making native call'
+    }
+}
+
 # Make sure a local Docker registry is running
 docker inspect registry | out-null
 if (!$?)
@@ -11,19 +20,26 @@ $PREFIX="localhost:5000/github-status-stats"
 
 $BASE="$PREFIX-base"
 docker build -t $BASE -f docker-base/Dockerfile .
+ExitIfNativeCallFailed $?
 
 $IMPORTER="$PREFIX-importer"
 docker build --build-arg REGISTRY=localhost:5000 -t ${IMPORTER} -f importer/Dockerfile .
+ExitIfNativeCallFailed $?
 
 $WEBSITE="$PREFIX-website"
 docker build --build-arg REGISTRY=localhost:5000 -t ${WEBSITE} -f website/Dockerfile .
+ExitIfNativeCallFailed $?
 
 docker push ${IMPORTER}
+ExitIfNativeCallFailed $?
 docker push ${WEBSITE}
+ExitIfNativeCallFailed $?
 
 # Deploy images
 kustomize edit set image `
     frigus02/github-status-stats-importer="$(docker inspect --format '{{json .RepoDigests}}' ${IMPORTER} | jq -r '.[0]')" `
     frigus02/github-status-stats-website="$(docker inspect --format '{{json .RepoDigests}}' ${WEBSITE} | jq -r '.[0]')"
+ExitIfNativeCallFailed $?
 
 kustomize build | kubectl apply -f -
+ExitIfNativeCallFailed $?
