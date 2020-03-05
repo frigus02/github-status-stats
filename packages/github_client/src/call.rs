11eeef08d8d1a024c4c675cb61aa6e54d5f9bfdb
Jan Kuehle <jkuehle90@gmail.com>
2020-03-05 19:56:21 +0000

    Unify request logging for GitHub and InfluxDB

--- packages/github_client/src/call.rs
+++ packages/github_client/src/call.rs
@@ -1,6 +1,6 @@
 use super::page_links;
 use reqwest::header::{ACCEPT, LINK};
-use reqwest::{Client, RequestBuilder, Url};
+use reqwest::{Client, Request, Url};
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::error::Error;
@@ -15,10 +15,11 @@ pub const MACHINE_MAN_PREVIEW: &str = "application/vnd.github.machine-man-previe
 pub const ANTIOPE_PREVIEW: &str = "application/vnd.github.antiope-preview+json";
 
 async fn call_api<T: DeserializeOwned>(
-    request: RequestBuilder,
+    client: &Client,
+    request: Request,
 ) -> Result<Response<T>, Box<dyn Error>> {
-    debug!(?request, "github request");
-    let res = request.send().await?.error_for_status()?;
+    debug!(request.method = %request.method(), request.url = %request.url(), request.body = ?request.body(), "github request");
+    let res = client.execute(request).await?.error_for_status()?;
     let next_page_url = match res.headers().get(LINK) {
         Some(value) => {
             if let Some(url) = page_links::parse(value.to_str()?).next {
@@ -38,7 +39,7 @@ async fn call_api<T: DeserializeOwned>(
 }
 
 pub async fn get<T: DeserializeOwned>(client: &Client, url: Url) -> Result<T, Box<dyn Error>> {
-    let result = call_api::<T>(client.get(url)).await?;
+    let result = call_api::<T>(client, client.get(url).build()?).await?;
     Ok(result.data)
 }
 
@@ -47,7 +48,7 @@ pub async fn post<B: Serialize + ?Sized, T: DeserializeOwned>(
     url: Url,
     body: &B,
 ) -> Result<T, Box<dyn Error>> {
-    let result = call_api::<T>(client.post(url).json(body)).await?;
+    let result = call_api::<T>(client, client.post(url).json(body).build()?).await?;
     Ok(result.data)
 }
 
@@ -56,7 +57,7 @@ pub async fn post_preview<T: DeserializeOwned>(
     url: Url,
     preview: &str,
 ) -> Result<T, Box<dyn Error>> {
-    let result = call_api::<T>(client.post(url).header(ACCEPT, preview)).await?;
+    let result = call_api::<T>(client, client.post(url).header(ACCEPT, preview).build()?).await?;
     Ok(result.data)
 }
 
@@ -68,7 +69,7 @@ pub async fn get_paged<T: DeserializeOwned>(
 
     let mut next_page_url = Some(url);
     while let Some(url) = next_page_url {
-        let result = call_api::<T>(client.get(url)).await?;
+        let result = call_api::<T>(client, client.get(url).build()?).await?;
         items.push(result.data);
         next_page_url = result.next_page_url;
     }
@@ -85,7 +86,8 @@ pub async fn get_paged_preview<T: DeserializeOwned>(
 
     let mut next_page_url = Some(url);
     while let Some(url) = next_page_url {
-        let result = call_api::<T>(client.get(url).header(ACCEPT, preview)).await?;
+        let result =
+            call_api::<T>(client, client.get(url).header(ACCEPT, preview).build()?).await?;
         items.push(result.data);
         next_page_url = result.next_page_url;
     }
--- packages/influxdb_client/src/lib.rs
+++ packages/influxdb_client/src/lib.rs
@@ -53,7 +53,7 @@ impl Client<'_> {
     pub async fn query(&self, q: &str) -> Result<QueryResponse, BoxError> {
         let raw_url = format!("{base}/query", base = &self.base_url);
         let url = Url::parse_with_params(&raw_url, &[("db", &self.db)])?;
-        debug!(request.url = %url, "influxdb request");
+        debug!(request.method = "POST", request.url = %url, request.body.q = q, "influxdb request");
         let result = self
             .client
             .post(url)
@@ -74,7 +74,7 @@ impl Client<'_> {
             .map(|point| point.to_line())
             .collect::<Vec<String>>()
             .join("\n");
-        debug!(request.url = %url, request.body = %body, "influxdb request");
+        debug!(request.method = "POST", request.url = %url, request.body = %body, "influxdb request");
         self.client
             .post(url)
             .body(body)
