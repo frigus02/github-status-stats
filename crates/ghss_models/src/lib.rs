c591f2932d93a1ace507fac8e2e4ea0f5127b60d
Jan Kuehle <jkuehle90@gmail.com>
2020-05-12 22:16:11 +0100

    Cleanup now unused models

--- crates/ghss_models/src/lib.rs
+++ crates/ghss_models/src/lib.rs
@@ -1,13 +1,9 @@
 mod build;
-mod commit;
 mod hook;
-mod import;
 
 pub use build::*;
-pub use commit::*;
 use ghss_github::Repository;
 pub use hook::*;
-pub use import::*;
 
 pub fn influxdb_name(repository: &Repository) -> String {
     format!("r{}", repository.id)
