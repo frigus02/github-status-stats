3fc1d0bf30e02042019f086abc48672822058734
Jan Kuehle <jkuehle90@gmail.com>
2020-07-08 22:02:29 +0100

    Use OpenTelemetry in store

--- crates/ghss_store/src/main.rs
+++ crates/ghss_store/src/main.rs
@@ -5,13 +5,15 @@ mod health;
 mod query;
 mod store;
 
-use ghss_tracing::{register_new_tracing_root, register_tracing_root};
 use health::{HealthServer, HealthService};
+use opentelemetry::api::{Provider, TraceContextPropagator};
 use proto::query_server::QueryServer;
 use proto::store_server::StoreServer;
 use std::convert::From;
 use tonic::{transport::Server, Code, Status};
 use tracing::info_span;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
+use tracing_subscriber::layer::SubscriberExt;
 
 pub(crate) mod proto {
     tonic::include_proto!("ghss.store");
@@ -44,15 +46,50 @@ impl SQLiteStore {
     }
 }
 
+fn init_tracer(agent_endpoint: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
+    let provider = match agent_endpoint {
+        Some(agent_endpoint) => {
+            let exporter = opentelemetry_jaeger::Exporter::builder()
+                .with_agent_endpoint(agent_endpoint.parse().unwrap())
+                .with_process(opentelemetry_jaeger::Process {
+                    service_name: "store".to_string(),
+                    tags: vec![],
+                })
+                .init()?;
+            let batch = opentelemetry::sdk::BatchSpanProcessor::builder(
+                exporter,
+                tokio::spawn,
+                tokio::time::interval,
+            )
+            .build();
+            opentelemetry::sdk::Provider::builder()
+                .with_batch_exporter(batch)
+                .build()
+        }
+        None => {
+            let exporter = opentelemetry::exporter::trace::stdout::Builder::default().init();
+            opentelemetry::sdk::Provider::builder()
+                .with_simple_exporter(exporter)
+                .build()
+        }
+    };
+
+    let tracer = provider.get_tracer("store");
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = tracing_subscriber::Registry::default().with(telemetry);
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    let propagator = TraceContextPropagator::new();
+    opentelemetry::global::set_http_text_propagator(propagator);
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::load();
 
-    ghss_tracing::setup(ghss_tracing::Config {
-        honeycomb_api_key: config.honeycomb_api_key.unsecure().to_owned(),
-        honeycomb_dataset: config.honeycomb_dataset.clone(),
-        service_name: "store",
-    });
+    init_tracer(config.otel_agent_endpoint.as_deref())?;
 
     let health_service = HealthService::default();
     let store = SQLiteStore {
@@ -61,24 +98,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Server::builder()
         .trace_fn(|headers| {
+            let cx = opentelemetry::global::get_http_text_propagator(|propagator| {
+                propagator.extract(headers)
+            });
             let span = info_span!("request");
-            {
-                // TODO: This seems weird. Need to understand why that's
-                // necessary or how to do it better.
-                let _guard = span.enter();
-                match (
-                    headers.get(ghss_tracing::HEADER_TRACE_ID),
-                    headers.get(ghss_tracing::HEADER_PARENT_SPAN_ID),
-                ) {
-                    (Some(trace_id), Some(parent_span_id)) => {
-                        register_tracing_root(
-                            trace_id.to_str().unwrap(),
-                            parent_span_id.to_str().unwrap(),
-                        );
-                    }
-                    _ => register_new_tracing_root(),
-                };
-            }
+            span.set_parent(&cx);
             span
         })
         .add_service(HealthServer::new(health_service))
